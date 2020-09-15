b552abb554064e7739ebac4841068503a1d27c2a
Renan de Faria <renandefaria2009@hotmail.com>
2020-09-15 20:58:25 -0300

    Atualizando as funçoes do projeto

--- script.js
+++ script.js
@@ -2,62 +2,70 @@ let h1 = document.getElementsByClassName("squad")
 let butt = document.getElementById("press")
 let butto = document.getElementById("pressi")
 
-console.log(h1)
-console.log(butt)
-    
-butt.addEventListener("click", all_cor)
-butto.addEventListener("click", vermelho)
 
-function vermelho(){
-    //teste para ativa todas essa div 
+butt.addEventListener("click", confirmar)
+butto.addEventListener("click", apagar)
+
+var arybuton = [false,false,false,false,false]
+
+function apagar(){
     let tes = document.querySelectorAll(['.squad'])
-    console.log('tes query',tes)
-    //modificar essa function
-    document.body.style.backgroundColor = "red";
+    tes = Array.from(tes)
+    tes.forEach(function(item,index ,arr){
+        item.removeAttribute("style")
+        arybuton[index] = false
+    })
 }
-function all_cor(){
-    l1.style.backgroundColor = "red"
-    l2.style.backgroundColor= "blue"
-    l3.style.backgroundColor= "yellow"
-    l4.style.backgroundColor= "green"
-    l5.style.backgroundColor= "purple"
+function confirmar(){
+    arybuton.forEach(function(item,index,arr){
+        if(arybuton[0,2,4] == true && arybuton[1,3] == false){
+            alert("Vitoria ,Parabens você conseguiu desvendar o Enigma ")
+        }
+    })
+}
 
-} 
-//    pega esse h1 e colocar no l1 e chamar uma fuction apos o click
-l1 = document.getElementById("primeiro")
-//    r1.onclick = mudarText;
-l1.onclick = mudarCor;   //se eu quiser muda a cor
+let senhatxt = document.getElementById("senha_mostrar") 
+let senhabutton = document.getElementById("senha") // tentar pega o valor desse input
+// talvez eu consiga se colocar na ativasao do button ok 
+senhabutton.onchange = function(){
+    senhatxt.innerText = senhabutton.value
+} //se quiser muda o texto
 
-//    function mudarText(){
-//        this.innerHTML = "Novo Texto";  //se quiser muda o texto
-//    }
 
-// pega esse h1 e colocar no l2 3 4 5 e chamar uma fuction apos o click
-l2 = document.getElementById("segundo")
+l1 = document.getElementById("colorsquad0")
+l1.onclick = mudarCor;
+
+l2 = document.getElementById("colorsquad1")
 l2.onclick = mudarCor2; // alem de onclick existem varios tipo onmouseover onmouseout
 
-l3 = document.getElementById("terceiro")
+l3 = document.getElementById("colorsquad2")
 l3.onclick = mudarCor3;
 
-l4 = document.getElementById("quarto")
+l4 = document.getElementById("colorsquad3")
 l4.onclick = mudarCor4;
 
-l5 = document.getElementById("quinto")
+l5 = document.getElementById("colorsquad4")
 l5.onclick = mudarCor5;
 
 // FUCTION De Muda de Cor
+
 function mudarCor(){
     this.style.backgroundColor= "red"
+    arybuton[0] = true
 }
 function mudarCor2(){
     this.style.backgroundColor= "blue"
+    arybuton[1] = true
 }
 function mudarCor3(){
     this.style.backgroundColor= "yellow"
+    arybuton[2] = true
 }
 function mudarCor4(){
     this.style.backgroundColor= "green"
+    arybuton[3] = true
 }
 function mudarCor5(){
     this.style.backgroundColor= "purple"
+    arybuton[4] = true
 }
